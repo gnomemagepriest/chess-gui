87831c94253c0acef1b60556fdd9100c01ddce67
gnomemagepriest <diedatsleepover344@gmail.com>
2024-12-03 10:39:56 +0700

    Добавлены базовые файлы проекта (С++)
    Добавлены файлы заголовков и описаний базовых файлов, файлы библиотеки SFML

---
+++ C++/Main.cpp
@@ -0,0 +1,9 @@
+﻿#include <SFML/Graphics.hpp>
+#include "GameScreen.h"
+
+int main()
+{
+    
+
+    return 0;
+}
---
+++ C++/Piece.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Piece
+{
+	sf::Sprite Sprite;
+	int x, y;
+};
+
