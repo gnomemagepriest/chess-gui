893afde4806be6eba7397497ae7720e56ebafbcd
gnomemagepriest <diedatsleepover344@gmail.com>
2024-12-03 11:29:20 +0700

    Добавлена отрисовка окна и обработка событий

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -3,7 +3,22 @@
 
 int main()
 {
-    
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Chess");
+
+    GameScreen gameScreen;
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            gameScreen.update(event);
+        }
+
+        window.clear();
+        gameScreen.draw(window);
+        window.display();
+    }
 
     return 0;
 }
